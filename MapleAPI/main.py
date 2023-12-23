70b0dcf7bf4f6258f0255a4027fd509330ff80fd
PizzaPark0 <85287012+PizzaPark0@users.noreply.github.com>
2023-12-24 05:17:23 +0900

    Update main.py

--- MapleAPI/main.py
+++ MapleAPI/main.py
@@ -4,7 +4,7 @@ import time
 with open("set.txt", "r") as f:
     apiKey = f.readline().split(':')[1].strip()
 
-viewer = maple_request.MapleAPI_Viewer(apiKey)
+viewer = maple_request.MapleAPI(apiKey)
 oGuildId = viewer.getOguildId("길드명", "서버명")
 guildMembers = viewer.getGuildData(oGuildId, "2023-12-22", "basic").get("guild_member")
 guildMembers_ocid = [  ]
